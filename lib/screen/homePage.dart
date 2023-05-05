9b57e2e3532b6a45c37a8dfd05800a423aba6b6d
michaelPi1912 <92928357+michaelPi1912@users.noreply.github.com>
2023-05-06 00:03:01 +0700

    update lai

--- lib/screen/homePage.dart
+++ lib/screen/homePage.dart
@@ -8,7 +8,7 @@ import '../network/networkApi.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
-
+  
   @override
   State<HomePage> createState() => _HomePageState();
 }
--- lib/screen/homeScreen.dart
+++ lib/screen/homeScreen.dart
@@ -15,55 +15,47 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
-  late List<Widget> _pages;
   int currentIndex = 0;
-  late List<BottomNavigationBarItem> _items;
-  late BottomNavigationBar bottomNavigationBar;
 
-
-  _updatePages(){
-    _pages = [
-      const HomePage(),
-      const CartPage(),
-      const ProfilePage(),
-    ];
-    _items = <BottomNavigationBarItem>[
-      const BottomNavigationBarItem(
-      icon: Icon(Icons.home),
-      label: 'Home',
-      backgroundColor: Colors.white,
-      ),
-      const BottomNavigationBarItem(
-      icon: Icon(Icons.add_shopping_cart),
-      label: 'Cart',
-      backgroundColor: Colors.white,
-      ),
-      const BottomNavigationBarItem(
-      icon: Icon(Icons.account_circle),
-      label: 'Profile',
-      backgroundColor: Colors.white,
-      ),
-    ];
-    bottomNavigationBar = BottomNavigationBar(
-      currentIndex: currentIndex,
-      onTap: (index) =>setState(() => currentIndex=index),
-      backgroundColor: Colors.amber,
-      items: _items);
-    assert(_pages.length == _items.length);
-  }
-  @override
-  void initState() {
-    _updatePages();
-    super.initState();
-  }
+  final pages = [
+    HomePage(),
+    CartPage(),
+    // NoticePage(),
+    ProfilePage(),
+  ];
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: IndexedStack( 
         index: currentIndex,
-        children: _pages,),
-        bottomNavigationBar: bottomNavigationBar
+        children: pages,),
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: currentIndex,
+        onTap: (index) =>setState(() => currentIndex=index),
+        backgroundColor: Colors.amber,
+        items: const [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: 'Home',
+            backgroundColor: Colors.white,
+            ),
+            BottomNavigationBarItem(
+            icon: Icon(Icons.add_shopping_cart),
+            label: 'Cart',
+            backgroundColor: Colors.white,
+            ),
+            // BottomNavigationBarItem(
+            // icon: Icon(Icons.circle_notifications),
+            // label: 'Home',
+            // backgroundColor: Colors.blueAccent,
+            // ),
+            BottomNavigationBarItem(
+            icon: Icon(Icons.account_circle),
+            label: 'Profile',
+            backgroundColor: Colors.white,
+            ),
+        ]),
     );
   }
 }
